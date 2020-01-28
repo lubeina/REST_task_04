4ea763055b11d206f0a5c5353df451ae27276749
lubeina <52536093+lubeina@users.noreply.github.com>
2020-01-28 17:40:13 +0300

    Task Complete

--- task_1/urls.py
+++ task_1/urls.py
@@ -24,6 +24,7 @@ from flights import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
+    path('register/', views.RegisterAPIView.as_view(), name="register"),
     
     path('bookings/', views.BookingsList.as_view(), name="bookings-list"), 
     path('booking/<int:booking_id>/', views.BookingDetails.as_view(), name="booking-details"),
